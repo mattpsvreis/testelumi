e1ce724b6fb3f354e36111e245283309be89e7e8
Mateus Reis <mateuspsvreis@gmail.com>
2024-10-18 16:46:56 -0300

    (frontend) feat: light and dark mode with tailwind and zustand

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,27 @@
+import { useEffect } from 'react';
+
+import { useThemeStore } from '@stores/themeStore';
+
 function App() {
+  const { theme, toggleTheme } = useThemeStore();
+
+  useEffect(() => {
+    if (theme === 'dark') {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [theme]);
+
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div className={theme}>
+      <button onClick={toggleTheme}>
+        Switch to {theme === 'light' ? 'Dark' : 'Light'} Mode
+      </button>
+      <h1 className={`text-light-primary dark:text-dark-secondary`}>
+        Hello World
+      </h1>
+    </div>
   );
 }
 
