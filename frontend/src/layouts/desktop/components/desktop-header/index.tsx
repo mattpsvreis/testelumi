76877719dffd7c530593232de570fc294712980c
Mateus Reis <mateuspsvreis@gmail.com>
2024-10-19 18:12:46 -0300

    (frontend) feat: super basic header for desktop

--- frontend/src/layouts/desktop/components/desktop-header/index.tsx
+++ frontend/src/layouts/desktop/components/desktop-header/index.tsx
@@ -1,7 +1,41 @@
-interface DesktopHeaderProps {}
+import { useAuthStore } from '@stores/AuthStore';
+import { useThemeStore } from '@stores/ThemeStore';
+import { Link, useNavigate } from 'react-router-dom';
 
-const DesktopHeader: React.FC<DesktopHeaderProps> = () => {
-  return <div>DesktopHeader</div>;
+interface DesktopHeaderProps {
+  shouldRender?: boolean;
+}
+
+const DesktopHeader: React.FC<DesktopHeaderProps> = ({
+  shouldRender = true,
+}) => {
+  const { theme, toggleTheme } = useThemeStore();
+  const { setUsername, setIsAuthenticated } = useAuthStore();
+
+  const navigate = useNavigate();
+
+  function handleLogout() {
+    setUsername('');
+    setIsAuthenticated(false);
+    localStorage.removeItem('isAuthenticated');
+    navigate('/login');
+  }
+
+  return shouldRender ? (
+    <div className='flex min-h-12 w-full flex-row items-center justify-between px-8'>
+      <div>
+        <Link to='/'>Home</Link>
+      </div>
+      <div className='flex flex-row gap-4'>
+        <button type='button' onClick={() => toggleTheme()}>
+          {theme.charAt(0).toUpperCase() + theme.slice(1).toLowerCase()} Mode
+        </button>
+        <button type='button' onClick={handleLogout}>
+          Logout
+        </button>
+      </div>
+    </div>
+  ) : null;
 };
 
 export default DesktopHeader;
--- frontend/src/layouts/desktop/index.tsx
+++ frontend/src/layouts/desktop/index.tsx
@@ -1,9 +1,21 @@
+import { useLocation } from 'react-router-dom';
+
+import DesktopHeader from './components/desktop-header';
+
 interface DesktopLayout {
   children: React.ReactNode;
 }
 
 const DesktopLayout: React.FC<DesktopLayout> = ({ children }) => {
-  return <div>{children}</div>;
+  const pathname = useLocation().pathname;
+
+  return (
+    <div className='flex min-h-screen w-full flex-col'>
+      {Boolean(pathname !== '/login') && <DesktopHeader />}
+      <hr />
+      {children}
+    </div>
+  );
 };
 
 export default DesktopLayout;
