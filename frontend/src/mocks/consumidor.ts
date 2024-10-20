19967108f8c0e835360b2321d3b48081a5245d51
Mateus Reis <mateuspsvreis@gmail.com>
2024-10-20 18:21:08 -0300

    (frontend) feat: mock selfway

--- frontend/src/mocks/consumidor.ts
+++ frontend/src/mocks/consumidor.ts
@@ -8,6 +8,12 @@ if (!faturasJoseMesaly) {
   throw new Error('Faturas do cliente 7204076116 não encontradas.');
 }
 
+const faturasSelfWay = await getFaturasByNumeroCliente('7202210726');
+
+if (!faturasSelfWay) {
+  throw new Error('Faturas do cliente 7202210726 não encontradas.');
+}
+
 export const consumidorMockJoseMesaly: Consumidor = {
   nome_uc: 'JOSE MESALY FONSECA DE CARVALHO 52024156',
   numero_cliente: '7204076116',
@@ -15,4 +21,13 @@ export const consumidorMockJoseMesaly: Consumidor = {
   distribuidora: 'CEMIG',
   consumidor: 'JOSE MESALY FONSECA DE CARVALHO 52024156',
   faturas: faturasJoseMesaly,
+};
+
+export const consumidorMockSelfway: Consumidor = {
+  nome_uc: 'SELFWAY TREINAMENTO PERSONALIZADO LTDA',
+  numero_cliente: '7202210726',
+  numero_uc: '3001422762',
+  distribuidora: 'CEMIG',
+  consumidor: 'SELFWAY TREINAMENTO PERSONALIZADO LTDA',
+  faturas: faturasSelfWay,
 };
--- frontend/src/pages/biblioteca/index.tsx
+++ frontend/src/pages/biblioteca/index.tsx
@@ -1,11 +1,14 @@
-import { consumidorMockJoseMesaly } from '@mocks/consumidor';
+import {
+  consumidorMockJoseMesaly,
+  consumidorMockSelfway,
+} from '@mocks/consumidor';
 
 import Table from './components/table';
 
 const Biblioteca: React.FC = () => {
   return (
     <div className='mt-8'>
-      <Table data={[consumidorMockJoseMesaly]} />
+      <Table data={[consumidorMockJoseMesaly, consumidorMockSelfway]} />
     </div>
   );
 };
